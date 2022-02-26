eee7a25c3e036bee1ceb175718e3738e12962286
Yota0350 <mori0124running@gmail.com>
2022-02-26 15:36:27 +0900

    first commit

---
+++ iOSStandardClockApp/iOSStandardClockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSStandardClockApp
+//
+//  Created by Yota on 2022/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
